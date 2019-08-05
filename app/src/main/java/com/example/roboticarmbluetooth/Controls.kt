6cd7be971da501c1b666eab0e1671abc34a159f7
Roow <40032541+Roow@users.noreply.github.com>
2019-08-05 22:52:31 +1000

    commit

--- app/src/main/java/com/example/roboticarmbluetooth/Controls.kt
+++ app/src/main/java/com/example/roboticarmbluetooth/Controls.kt
@@ -10,6 +10,8 @@ class Controls : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_controls)
 
+        seekBar1.max = 120
+
         seekBar1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
 
@@ -24,5 +26,114 @@ class Controls : AppCompatActivity() {
             }
 
         })
+
+        seekBar2.max = 120
+
+        seekBar2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        seekBar3.max = 180
+
+        seekBar3.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        seekBar4.max = 180
+
+        seekBar4.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        seekBar5.max = 180
+
+        seekBar5.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+
+            }
+
+        })
+
+        seekBar6.max = 180
+
+        seekBar6.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+        seekBar7.max = 90
+
+        seekBar7.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
+
+        home_button.setOnClickListener {
+            seekBar1.progress = 0
+            seekBar2.progress = 0
+            seekBar3.progress = 0
+            seekBar4.progress = 0
+            seekBar5.progress = 0
+            seekBar6.progress = 0
+            seekBar7.progress = 0
+
+        }
     }
 }
--- app/src/main/java/com/example/roboticarmbluetooth/DeviceSelection.kt
+++ app/src/main/java/com/example/roboticarmbluetooth/DeviceSelection.kt
@@ -1,13 +1,80 @@
 package com.example.roboticarmbluetooth
 
+import android.app.Activity
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.Intent
+import android.util.Log
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import kotlinx.android.synthetic.main.device_selection.*
 
 class DeviceSelection : AppCompatActivity() {
+
+    var btAdapter: BluetoothAdapter? = null
+    lateinit var pairedDevice: Set<BluetoothDevice>
+    val requestBluetooth = 1
+
+    companion object{
+        val extraAddress: String = "Device_address"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.device_selection)
+
+        btAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (btAdapter == null){
+            Toast.makeText(this, "Does not support bluetooth", Toast.LENGTH_LONG).show()
+            return
+        }
+        if (!btAdapter!!.isEnabled){
+            val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBluetoothIntent, requestBluetooth)
+        }
+
+        device_selection_refresh.setOnClickListener { deviceList() }
+
+
+    }
+
+    private fun deviceList(){
+        pairedDevice = btAdapter!!.bondedDevices
+        val list : ArrayList<BluetoothDevice> = ArrayList()
+
+        if (pairedDevice.isNullOrEmpty()){
+            for (device: BluetoothDevice in pairedDevice){
+                list.add(device)
+                Log.i("devices", ""+device)
+            }
+        } else {
+            Toast.makeText(this, "No device found", Toast.LENGTH_LONG).show()
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == requestBluetooth) {
+            if (resultCode == Activity.RESULT_OK){
+                if (btAdapter!!.isEnabled) {
+                    Toast.makeText(this, "Enabled", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Not enabled", Toast.LENGTH_LONG).show()
+                }
+            } else if (resultCode==Activity.RESULT_CANCELED){
+                Toast.makeText(this, "bluetooth enabling is cancelled", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
